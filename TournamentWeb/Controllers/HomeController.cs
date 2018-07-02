02ae6b8532c80c28c0c95853d3723c5f5c362e09
Jan Odegard <jan.odegard@novanet.no>
2018-07-02 22:23:08 +0200

    Fix build

--- TournamentWeb/Controllers/HomeController.cs
+++ TournamentWeb/Controllers/HomeController.cs
@@ -9,7 +9,6 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
-using TournamentWeb.Models;
 
 namespace TournamentWeb.Controllers
 {

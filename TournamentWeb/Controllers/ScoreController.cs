e79516847e4804a7c7609ba518ac523eb0f196ef
Jan Odegard <jan.odegard@novanet.no>
2018-07-02 22:47:33 +0200

    Add score controller

---
+++ TournamentWeb/Controllers/ScoreController.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TournamentWeb.Controllers
+{
+    public class ScoreController : Controller
+    {
+        [Authorize]
+        public async Task<IActionResult> Process()
+        {
+            return View();
+        }
+    }
+}
